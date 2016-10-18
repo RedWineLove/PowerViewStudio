05f6bd043597395687013eff74128dea1bc57235
RedWineLove <RedWineLove@REDWINELOVE-PC>
2016-10-18 16:09:43 +0900

    asdasdasdasd

---
+++ SearchService/Program.cs
@@ -0,0 +1,79 @@
+﻿using CommonInterface;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchService
+{
+    class Program
+    {
+        static EndpointAddress serviceAddress;
+        static void Main(string[] args)
+        {
+            if (FindService())
+                InvokeService();
+        }
+
+        // ** DISCOVERY ** //
+        static bool FindService()
+        {
+
+            Console.WriteLine("\nFinding Calculator Service ..");
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            DiscoveryClient discoveryClient =
+                new DiscoveryClient(new UdpDiscoveryEndpoint());
+
+            var calculatorServices =                
+                discoveryClient.Find(new FindCriteria(typeof(IHelloWorldService)));
+
+            discoveryClient.Close();
+
+            Console.WriteLine("Find Completed: " + sw.Elapsed);
+
+            if (calculatorServices.Endpoints.Count == 0)
+            {
+                Console.WriteLine("\nNo services are found.");
+                return false;
+            }
+            else
+            {
+                serviceAddress = calculatorServices.Endpoints[0].Address;
+                return true;
+            }
+
+            
+        }
+
+        static void InvokeService()
+        {
+            Console.WriteLine("Invoke Service");
+            System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+            ChannelFactory<IHelloWorldService> myChannelFactory = null;
+            try
+            {
+                Console.WriteLine("\nInvoking Calculator Service at {0}\n", serviceAddress);
+                BasicHttpBinding myBinding = new BasicHttpBinding();
+                myChannelFactory = new ChannelFactory<IHelloWorldService>(myBinding, serviceAddress);
+                IHelloWorldService service = myChannelFactory.CreateChannel();
+                string requestMesg = service.SayHello("My Name is " + System.Net.Dns.GetHostName());
+                Console.WriteLine("Result: " + requestMesg);
+                myChannelFactory.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                myChannelFactory.Abort();
+            }
+            finally
+            {
+                Console.WriteLine("Invoke Completed: " + sw.Elapsed);
+            }
+        }
+    }
+    
+}
---
+++ WCFServiceHost/Program.cs
@@ -0,0 +1,47 @@
+﻿using CommonInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri(string.Format("http://{0}:8000/discovery/scenarios/calculatorservice/{1}/",
+        System.Net.Dns.GetHostName() , Guid.NewGuid().ToString()));
+
+            // Create a ServiceHost for the CalculatorService type.
+            using (ServiceHost serviceHost = new ServiceHost(typeof(HelloWorldService), baseAddress))
+            {
+                // add calculator endpoint
+                serviceHost.AddServiceEndpoint(typeof(IHelloWorldService), new BasicHttpBinding(), string.Empty);
+
+                // ** DISCOVERY ** //
+                // make the service discoverable by adding the discovery behavior
+                ServiceDiscoveryBehavior discoveryBehavior = new ServiceDiscoveryBehavior();
+                serviceHost.Description.Behaviors.Add(discoveryBehavior);
+
+                // send announcements on UDP multicast transport
+                discoveryBehavior.AnnouncementEndpoints.Add(
+                  new UdpAnnouncementEndpoint());
+
+                // ** DISCOVERY ** //
+                // add the discovery endpoint that specifies where to publish the services
+                serviceHost.Description.Endpoints.Add(new UdpDiscoveryEndpoint());
+
+                // Open the ServiceHost to create listeners and start listening for messages.
+                serviceHost.Open();
+
+                // The service can now be accessed.
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
